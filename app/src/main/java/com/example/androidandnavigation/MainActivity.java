172d4cc2294d6aa80d0b6a5850230c8eff4b4e9b
Kiphea <djsmyth05@gmail.com>
2025-11-12 07:52:55 +0000

    Week 3 - Task 3 Complete

--- app/src/main/java/com/example/androidandnavigation/MainActivity.java
+++ app/src/main/java/com/example/androidandnavigation/MainActivity.java
@@ -1,11 +1,15 @@
 package com.example.androidandnavigation;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -23,5 +27,23 @@ public class MainActivity extends AppCompatActivity {
                 hello.setText("Goodbye!");
             }
         });
+    }
+
+    public void DialPhone(View view){
+        String number = "123456789";
+        if (checkPermission("android.permission.CALL_PHONE")==false){
+            Toast toast = Toast.makeText(this, "No Permissions to Call", Toast.LENGTH_SHORT);
+            toast.show();
+        }
+        else{
+            Intent intent = new Intent(Intent.ACTION_CALL);
+            intent.setData(Uri.parse("tel: " + number));
+            startActivity(intent);
+        }
+    }
+
+    private boolean checkPermission(String permission){
+        int permissionCheck = ContextCompat.checkSelfPermission(this, permission);
+        return(permissionCheck == getPackageManager().PERMISSION_GRANTED);
     }
 }
